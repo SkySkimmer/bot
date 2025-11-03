f81d4d4eefd9cf1cf1a2518314e0c632bcf8d9ba
kim.ly <kim.ly@telecom-paris.fr>
2025-11-03 11:19:41 +0100

    chore: move functions string manipulation from Utils to String_utils

---
+++ bot-components/String_utils.mli
@@ -0,0 +1,18 @@
+val string_match : regexp:string -> ?pos:int -> string -> bool
+
+val fold_string_matches :
+  regexp:string -> f:((unit -> 'a) -> 'a) -> init:'a -> ?pos:int -> string -> 'a
+
+val map_string_matches : regexp:string -> f:(unit -> 'a) -> string -> 'a list
+
+val iter_string_matches : regexp:string -> f:(unit -> unit) -> string -> unit
+
+val first_line_of_string : string -> string
+
+val remove_between : string -> int -> int -> string
+
+val code_wrap : string -> string
+
+val trim_comments : string -> string
+
+val strip_quoted_bot_name : github_bot_name:string -> string -> string
--- bot-components/Utils.mli
+++ bot-components/Utils.mli
@@ -9,22 +9,3 @@ val api_json_header : (string * string) list
 val toml_of_string : string -> Toml.Types.table
 
 val subkey_value : Toml.Types.table -> string -> string -> string option
-
-val code_wrap : string -> string
-
-val string_match : regexp:string -> ?pos:int -> string -> bool
-
-val fold_string_matches :
-  regexp:string -> f:((unit -> 'a) -> 'a) -> init:'a -> ?pos:int -> string -> 'a
-
-val map_string_matches : regexp:string -> f:(unit -> 'a) -> string -> 'a list
-
-val iter_string_matches : regexp:string -> f:(unit -> unit) -> string -> unit
-
-val first_line_of_string : string -> string
-
-val remove_between : string -> int -> int -> string
-
-val trim_comments : string -> string
-
-val strip_quoted_bot_name : github_bot_name:string -> string -> string
