8dff2d4a48693d8f0ca1d9baa8db0f9b30bc77e9
kim.ly <kim.ly@telecom-paris.fr>
2025-11-03 09:05:13 +0100

    fix: replace deprecated function

--- src/bot.ml
+++ src/bot.ml
@@ -659,6 +659,6 @@ let () =
       |> Lwt.async
 
 (* RNG seeding: https://github.com/mirage/mirage-crypto#faq *)
-let () = Mirage_crypto_rng_lwt.initialize (module Mirage_crypto_rng.Fortuna)
+let () = Mirage_crypto_rng_unix.use_default ()
 
 let () = Lwt_main.run launch
